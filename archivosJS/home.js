ffa22718906e2ee58b88a097c3025719ef71b0fd
jhoselerodriguez <jhoselerodriguez@gmail.com>
2024-08-11 16:02:47 -0500

    guardado

--- archivosJS/home.js
+++ archivosJS/home.js
@@ -1,5 +1,5 @@
 
-import {agentes} from "./Apis.js"
+import {agentes,mapas,armas,Ranked} from "./Apis.js"
 
 let Direc = agentes
 
@@ -8,23 +8,75 @@ let {createApp} = Vue
 let app = createApp({
     data(){
         return{
-
+            personajes:[],
+            Mapas:[],
+            Armas:[],
+            Estadisticas:[]
         }
     },
     created(){
         this.traerData(Direc)
+        this.traerMapas(mapas)
+        this.traerArmas(armas)
+        this.traerEst(Ranked)
     },
     methods:{
         traerData(url){
             fetch(url)
             .then(response => response.json())
-            .then(data=>{
-                console.log(data);
+            .then(dato=>{
+
+                this.personajes = dato["data"][0].fullPortrait
+                console.log(this.personajes);
+                
+            })
+        },
+        traerMapas(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(dato=>{
+
+                this.Mapas = dato["data"][0].splash
+                console.log(this.Mapas);
+                
+            })
+        },
+        traerArmas(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(dato=>{
+
+                this.Armas = dato["data"][0].displayIcon
+
+                
+            })
+        },
+        traerEst(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(dato=>{
+
+                this.Estadisticas = dato["data"][0].tiers[24].largeIcon
+                console.log(this.Estadisticas);
                 
             })
         }
+
+        
     },
     computed:{
 
     }
 }).mount("#app")
+
+let boton = document.querySelector(".lista-desplegable")
+function menuDesp(){
+    let menu = document.querySelector(".menu-desplegable")
+    let menuTotal = document.querySelector(".menu-total")
+    
+    menu.classList.toggle("left-0-menu")
+    menuTotal.classList.toggle("left-0-menu-total")
+    
+
+}
+boton.addEventListener("click", menuDesp)
